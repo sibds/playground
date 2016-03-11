e98514d206aba7233bc0aafa371665627d765d1b
Vadim Mazur <mazurva@gmail.com>
2016-03-11 16:42:12 +0600

    Update

--- modules/news2/Module.php
+++ modules/news2/Module.php
@@ -12,6 +12,8 @@ class Module extends \yii\base\Module
     */
     public $controllerNamespace = 'app\modules\news2\controllers';
 
+    public $defaultRoute = 'news/list';
+
     /**
     * @inheritdoc
     */
